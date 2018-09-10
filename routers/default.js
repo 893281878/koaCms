eff8f7c17e6b3f06aaa4e0fb076f6ee905cad321
Felix_f <893281878@qq.com>
2018-09-10 18:14:27 +0800

    创建数据表页面开发

--- routers/default.js
+++ routers/default.js
@@ -1,7 +1,11 @@
 const router = require('koa-router')();
+const mongoDB = require('../module/db');
 
 // router.prefix('/')
 router.get('/',async (ctx)=>{
-	await ctx.render('default')
+
+	let fieldsTypeList = await mongoDB.find('fields_type',{});
+	await ctx.render('admin/index',{fieldsTypeList});//default
+
 });
 module.exports = router;
